5165145c403cb1d0ce5144726fd035fb99464aa6
arsesecurity-hash <arse.security@gmail.com>
2026-08-31 16:22:50 +0500

    Copy core/serial_commands/cli.h into esp32s3-devkit

---
+++ boards/esp32s3-devkit/core/serial_commands/cli.h
@@ -0,0 +1,21 @@
+#ifndef __SERIAL_CLI_H__
+#define __SERIAL_CLI_H__
+
+#include <Arduino.h>
+#include <SimpleCLI.h>
+
+class SerialCli {
+public:
+    SerialCli();
+    void setup(void);
+
+    SimpleCLI getCli() { return _cli; };
+    bool parse(const String &input) { return _cli.parse(input); }
+
+private:
+    SimpleCLI _cli;
+};
+
+void cliErrorCallback(cmd_error *e);
+
+#endif
